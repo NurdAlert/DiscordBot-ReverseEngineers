be8e0950aacb3bf94a38518e0bcc1b4aa3b17b6d
JokinAce <jokinace@pornhubstaff.com>
2021-02-09 21:57:15 +0100

    Deleted shit

--- DiscordBotEthan/Program.cs
+++ DiscordBotEthan/Program.cs
@@ -71,7 +71,7 @@ namespace DiscordBotEthan {
 
         private static async Task MainAsync() {
             discord = new DiscordClient(new DiscordConfiguration {
-                Token = "",
+                Token = "", // Don't forget to delete this in push
                 TokenType = TokenType.Bot,
                 MinimumLogLevel = LogLevel.Information,
                 Intents = DiscordIntents.GuildMembers | DiscordIntents.AllUnprivileged
@@ -155,7 +155,7 @@ namespace DiscordBotEthan {
                     case DSharpPlus.CommandsNext.Exceptions.ChecksFailedException _:
                         await args.Context.RespondAsync("The FBI has been contacted (You don't have **the** (Thx Sven for correction) rights for that **c**ommand (Another correction))");
                         break;
-                }
+                } 
             };
 
             commands.SetHelpFormatter<CustomHelpFormatter>();
