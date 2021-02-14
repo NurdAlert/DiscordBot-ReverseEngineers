b4c0cf33ce7777a36a08783c90b30e4e6925c98e
JokinAce <jokinace@pornhubstaff.com>
2021-02-15 00:15:40 +0100

    Update Hackban.cs

--- DiscordBotEthan/Commands/Hackban.cs
+++ DiscordBotEthan/Commands/Hackban.cs
@@ -9,12 +9,12 @@ namespace DiscordBotEthan.Commands {
     public class Hackban : BaseCommandModule {
 
         [Command("Hackban"), RequirePermissions(DSharpPlus.Permissions.Administrator), Description("Bans the User without them being needed on the Server")]
-        public async Task HackbanCommand(CommandContext ctx, [Description("The User to ban (ID, Mention)")] DiscordUser user, [RemainingText, Description("Reason for the Ban")] string reason) {
+        public async Task HackbanCommand(CommandContext ctx, [Description("The User to ban (ID, Mention)")] DiscordUser user, [RemainingText, Description("Reason for the Ban")] string reason = "No reason specified") {
             await ctx.Guild.BanMemberAsync(user.Id, 1, reason);
 
             DiscordEmbedBuilder Hackban = new DiscordEmbedBuilder {
                 Title = $"Hackban | {user.Username}",
-                Description = $"{user.Mention} **has been banned from the Server without being on it for the following Reason:**\n{reason}",
+                Description = $"{user.Mention} **has been banned from the Server for the following Reason:**\n{reason}",
                 Color = Program.EmbedColor,
                 Footer = new DiscordEmbedBuilder.EmbedFooter { Text = "Made by JokinAce 😎" },
                 Timestamp = DateTimeOffset.Now
